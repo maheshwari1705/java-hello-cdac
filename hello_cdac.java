1a89f7bb4cb5c3d8ef0e936d2f183f4ef75fcbd3
maheshwari1705 <maheshwaribhalshankar1705@gmail.com>
2024-04-27 01:10:25 +0530

    hello-cdac

--- hello_cdac.java
+++ hello_cdac.java
@@ -2,6 +2,7 @@
 
 class hello_cdac {
     public static void main(String[] args) {
-        System.out.println("Hello C-DAC"); 
+        System.out.println("Hello C-DAC");
+        System.out.println("Hello from  C-DAC ");	
     }
 }
